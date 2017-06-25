442d29c2b7d4f5d0ae77e1dd904905d8e666b771
Behnam Baharmand <bbaharmand@gmail.com>
2017-06-25 16:27:42 +0430

    Start main.js

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,3 +1,15 @@
+// TODO: Refactor via ES6
+
 // Require Styles
 require("./style.css");
 
+var gistForm = document.getElementById("gistForm");
+
+gistForm.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  var gistInput = document.getElementById("txtInput").value;
+
+  localStorage.setItem("gists", gistInput);
+  
+});
