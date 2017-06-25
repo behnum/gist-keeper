bfc38f6d8251355426573d13d3402499ff8b72f7
Behnam Baharmand <behnam@baharmand.com>
2017-06-25 14:33:03 +0430

    Add webpack-dev-server and config

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+var webpack           = require('webpack');
+var path              = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: "./src/assets/main.js",
+
+  output: {
+    path: path.resolve(__dirname, "./dist"),
+    filename: "assets/scripts.bundle.js"
+  },
+
+  devtool: 'source-map',
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          { loader: "style-loader" },
+          { loader: "css-loader" }
+        ]
+      }
+    ]
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      "title": "Webpack Starter",
+      minify: {
+        collapseWhitespace: true
+      },
+      hash: true,
+      template: "./src/index.ejs"
+    })
+  ]
+};
