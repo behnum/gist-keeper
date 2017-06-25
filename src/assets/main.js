b61639c2cdedc4d55985f04595c9cb19c974ef7e
Behnam Baharmand <behnam@baharmand.com>
2017-06-25 17:54:05 +0430

    Add basic functionality and update view

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,15 +1,67 @@
+// TODO: Make Modular
 // TODO: Refactor via ES6
 
 // Require Styles
 require("./style.css");
 
 var gistForm = document.getElementById("gistForm");
+gistForm.addEventListener("submit", saveGist);
 
-gistForm.addEventListener("submit", function (e) {
+updateView();
+
+// Save Gist
+function saveGist(e) {
   e.preventDefault();
 
   var gistInput = document.getElementById("txtInput").value;
 
-  localStorage.setItem("gists", gistInput);
-  
-});
+  var today = new Date(); // FIXME: Pretty dates
+  var gist = {
+    "txt": gistInput,
+    "date": today
+  };
+
+  var gists = [];
+
+  gists.push(gist);
+
+  if (!localStorage.getItem("gists")) { // First time init
+    localStorage.setItem("gists", JSON.stringify(gists));
+    updateView();
+  } else { // Already exists
+    // Load gists
+    var currentGists = JSON.parse(localStorage.getItem("gists"));
+
+    // Append to current gist
+    currentGists.push(gist);
+
+    // Update LocalStorage with gists
+    localStorage.setItem("gists", JSON.stringify(currentGists));
+
+    // Update View
+    updateView();
+
+  }
+}
+
+function updateView() {
+
+  if (localStorage.getItem("gists")) { // If exists
+    var gistsContainer = document.getElementById("gistsContainer");
+    gistsContainer.innerHTML = '';
+    
+    var gistsCount = document.getElementById("gistsCount");
+    gistsCount.innerHTML = JSON.parse(localStorage.getItem("gists")).length;
+
+
+    var gists = JSON.parse(localStorage.getItem("gists"));
+
+    for (i = 0; i < gists.length; i++) {
+      gistsContainer.innerHTML += gists[i].date + "<hr>";
+      gistsContainer.innerHTML += gists[i].txt + "<br><br>";
+
+      // TODO: Add Remove Button
+    }
+  }
+}
+
